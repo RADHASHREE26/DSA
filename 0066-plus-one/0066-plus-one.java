50891d1a47d812c13c127952dfdf4dac62b538c1
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-05 21:33:51 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (54.30%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,7 +1,30 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-        ArrayList<Integer> al = new ArrayList<>();
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]!=9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i]=0;
+        }
+        
+        digits = new int[digits.length+1];
+        
+        digits[0]=1;
+        return digits;
+        
+        
+    }
+}
+
+
+/*
+
+ArrayList<Integer> al = new ArrayList<>();
         int carry=0;
         int sum=1;
         for(int i=digits.length-1;i>=0;i--)
@@ -22,7 +45,5 @@ class Solution {
         
         return ar;
         
-    }
-}
-
+        */
        

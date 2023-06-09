2aeeeb97b4c9e739f5765925a96a9c9e47e67b66
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-09 23:37:36 +0530

    Time: 0 ms (100.00%), Space: 41 MB (56.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,29 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        
-        /*
+
+        double ans=1.0;
         long n1=n;
         n1=Math.abs(n1);
         
-        if (n == 0) {
-            return 1;
+        while(n1>0)
+        {
+            if(n1%2==0)
+            {
+                x=x*x;
+                n1=n1/2;
+            }
+            else{
+                ans=ans*x;
+                n1=n1-1;
+            }
         }
-        
-        if (n < 0) return 1 / myPow(x, -n);
-        
-        if (n % 2 == 0) 
-            return myPow(x * x, n / 2);
-        else 
-            return x * myPow(x,n-1);
-        */
-        
-        if (n == 0) {
-            return 1;
-        }
-        
-        if (n % 2 == 0) return myPow(x * x, n / 2);
-        else if (n % 2 == 1) return x * myPow(x, n - 1);
-        else return 1/myPow(x,-n);
+        if(n<0)
+            return 1.0/ans;
+        return ans;
         
     }
     
@@ -65,4 +61,16 @@ double ans=1.0;
         if(n<0)
             return 1.0/ans;
         return ans;
+        */
+
+
+/*
+        
+        if (n == 0) {
+            return 1;
+        }
+        
+        if (n % 2 == 0) return myPow(x * x, n / 2);
+        else if (n % 2 == 1) return x * myPow(x, n - 1);
+        else return 1/myPow(x,-n);
         */

dbae4ba5dc56ac91d1d58b7c4fc89ccc4b916674
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-13 00:06:03 +0530

    Time: 3 ms (97.75%), Space: 42.3 MB (82.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        String n="";
-        String n2="";
         s=s.toLowerCase();
-        for(int i=0;i<s.length();i++)
+        
+        int a=0;
+        int b=s.length()-1;
+        
+        
+        
+        while(a<=b)
         {
-            int a=(int)(s.charAt(i));
-            if((a>=97 && a<=122) || (a>=48 && a<=57))
+            if(!((s.charAt(a)>=97 && s.charAt(a)<=122) || (s.charAt(a)>=48 && s.charAt(a)<=57)))
             {
-            n=n+s.charAt(i);
-            n2=s.charAt(i)+n2;
+                a++;
+                continue;
             }
+            else if(!((s.charAt(b)>=97 && s.charAt(b)<=122) || (s.charAt(b)>=48 && s.charAt(b)<=57)))
+            {
+                b--;
+                continue;
+            }
+            else
+            {
+                if(s.charAt(a)!=s.charAt(b))
+                return false;
+            }
+            a++;
+            b--;
         }
-        if(n.equals(n2))
         return true;
-        else
-        return false;
         
     }
 }

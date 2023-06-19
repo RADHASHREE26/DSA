88fcab0b3bc2e0c8242185ce89af97daca214628
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-19 23:26:29 +0530

    Time: 5 ms (61.20%), Space: 42 MB (91.30%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,6 +4,32 @@ class Solution {
         if(s.length()!=t.length())
             return false;
         
+        int c[]=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            int n1=s.charAt(i)-'a';
+            int n2=t.charAt(i)-'a';
+            c[n1]++;
+            c[n2]--;
+        }
+        
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]!=0)
+                return false;
+        }
+        return true;
+        
+        
+    }
+}
+
+/*
+BRUTE-FORCE
+
+if(s.length()!=t.length())
+            return false;
+        
         for(int i=0;i<s.length();i++)
         {
             int n=t.indexOf(s.charAt(i));
@@ -14,5 +40,4 @@ class Solution {
         }
         return true;
         
-    }
-}
+        */

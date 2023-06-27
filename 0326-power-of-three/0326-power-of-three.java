6cbddf694edce13982311be0b3bef19d61afb4a3
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-28 00:24:59 +0530

    Time: 17 ms (13.45%), Space: 43.3 MB (9.69%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,8 +1,20 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
+        
         if(n==0)
             return false;
-        while(n%3==0)
+        double res=Math.log10(n)/Math.log10(3);
+        if(res==Math.ceil(res))
+            return true;
+        else
+            return false;
+        
+    }
+}
+
+/*
+
+while(n%3==0 && n>=1)
         {
             n/=3;
         }
@@ -10,6 +22,20 @@ class Solution {
             return true;
         else
             return false;
+            
+            */
+
+/*
+
+int n1=31;
+        while(Math.pow(2,31)<Math.pow(3,n1))
+        {
+            n1--;
+        }
         
-    }
-}
+        if(n>0 && n1%n==0)
+            return true;
+        else
+            return false;
+            
+            */

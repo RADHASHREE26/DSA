1e2ff94ce27f9efb5087b90a66016bdf2e6641a2
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-30 00:11:57 +0530

    Time: 0 ms (100.00%), Space: 56.1 MB (63.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,36 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         
+        k = k % nums.length;
         
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        
+    }
+    
+    public void reverse (int[] arr, int i, int j) {
+        
+        int l = i, r = j;
+        
+        while (l < r) {
+            
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            
+            l++;
+            r--;
+            
+        }
+
+        
+    }
+}
+
+/*
+
+ 
         int n=nums.length;
         k=k%n;
         
@@ -27,8 +56,4 @@ class Solution {
             nums[j]=temp;
         }
         
-    }
-    
-
-    
-}
+        */

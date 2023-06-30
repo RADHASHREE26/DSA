8b6be4b7e21d3b9bc8b40dc2a39fce2019196d3c
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-06-30 23:11:00 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (51.87%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,7 +19,7 @@ class Solution {
         if(root==null)
             return 0;
         else
-            return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+            return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
         
     }
 }
@@ -31,3 +31,12 @@ if(root==null)
         return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
         
         */
+
+/*
+
+if(root==null)
+            return 0;
+        else
+            return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+            
+            */

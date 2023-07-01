fff5d521d868f0146b4a964dea238436ef7f684e
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-07-02 00:05:39 +0530

    Time: 28 ms (21.24%), Space: 19.6 MB (56.32%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+        
+        unordered_map<int,int> hm;
+        
+        int n=arr.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(hm.find(arr[i])==hm.end())
+                hm[arr[i]]=1;
+            else
+                hm[arr[i]]=hm[arr[i]]+1;
+        }
+        
+        int no_of_occ=n/2;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(hm[arr[i]]>no_of_occ)
+                return arr[i];
+        }
+        
+        return -1;
+        
+    }
+};

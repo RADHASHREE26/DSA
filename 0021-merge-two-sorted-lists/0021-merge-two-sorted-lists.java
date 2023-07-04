4bfee29263fdfd91228dee116e6b9656110a9fcf
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-07-05 01:38:47 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (89.40%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,217 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        
+        ListNode dummy=new ListNode(0,null);
+        ListNode temp=dummy;
+        
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<list2.val)
+            {
+                temp.next=list1;
+                list1=list1.next;
+            }
+            else
+            {
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
+        }
+        
+        if(list1!=null)
+        {
+            temp.next=list1;
+        }
+        
+        if(list2!=null)
+        {
+            temp.next=list2;
+        }
+        
+        return dummy.next;
+        
+    }
+}
+
+
+/*
+EFFICIENT METHOD
+
+if(list1==null)
+        return list2;
+        if(list2==null)
+        return list1;
+        
+        ListNode d=new ListNode(0,null);
+        ListNode temp=d;
+
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<list2.val)
+            {
+                temp.next=list1;
+                list1=list1.next;
+            }
+            else
+            {
+                temp.next=list2;
+                list2=list2.next;
+            }
+            temp=temp.next;
+        }
+        if(list1!=null)
+        {
+            temp.next=list1;
+        }
+        if(list2!=null)
+        {
+            temp.next=list2;
+        }
+        return d.next;
+
+*/
+
+
+
+/* USING EXTRA SPACE
+o(n1+n2)
+
+ListNode d=new ListNode(0,null);
+        ListNode dd=d;
+        while(list1!=null && list2!=null)
+        {
+            if(list1.val<list2.val)
+            {
+                    ListNode n=new ListNode(list1.val,null);
+                    list1=list1.next;
+                    dd.next=n;
+                    dd=n;
+            }
+            else
+            {
+                    ListNode n=new ListNode(list2.val,null);
+                    list2=list2.next;
+                    dd.next=n;
+                    dd=n;
+            }
+        }
+        while(list1!=null)
+        {
+            ListNode n=new ListNode(list1.val,null);
+            list1=list1.next;
+            dd.next=n;
+            dd=n;
+        }
+        while(list2!=null)
+        {
+            ListNode n=new ListNode(list2.val,null);
+            list2=list2.next;
+            dd.next=n;
+            dd=n;
+        }
+        return d.next;
+        
+
+        */
+
+
+        /*if(list1.val<list2.val)
+            {
+                temp=list1;
+                list1=list1.next;
+            }
+            else
+            {
+                temp.next=list2;
+                ListNode n=list1;
+                list1=list2;
+                list2=n;
+            }*/
+
+/*
+
+if (list1 == null && list2 == null) return null;
+        if (list1 == null && list2 != null) return list2;
+        if (list2 == null && list1 != null) return list1;
+        
+        ListNode i = list1, j = list2;
+        
+        ListNode head = null, tail = null;
+        
+        while (i != null && j != null) {
+            
+            if (i.val <= j.val) {
+                
+                if (head == null) {
+                    
+                    head = i;
+                    tail = head;
+                    
+                }
+                else {
+                    
+                    tail.next = i;
+                    tail = tail.next;
+                    
+                }
+                i = i.next;
+                
+            }
+            else {
+                
+                if (head == null) {
+                    
+                    head = j;
+                    tail = head;
+                    
+                }
+                else {
+                    
+                    tail.next = j;
+                    tail = tail.next;
+                    
+                }
+                
+                j = j.next;
+                
+            }
+            
+        }
+        
+        while (i != null) {
+            
+            tail.next = i;
+            tail = tail.next;
+            
+            i = i.next;
+            
+        }
+
+        while (j != null) {
+            
+            tail.next = j;
+            tail = tail.next;
+            
+            j = j.next;
+            
+        }
+        
+        return head;
+        
+        */

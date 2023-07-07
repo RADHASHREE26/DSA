d958a5c50221cd09d495aa2fd465a4f575d4ee4e
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-07-07 17:46:31 +0530

    Time: 987 ms (5.01%), Space: 47.2 MB (31.80%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,63 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        List<List<String>> res=new ArrayList<>();
+        
+        for(int i=0;i<strs.length;i++)
+        {
+            if(res.isEmpty())
+            {
+                List<String> r1=new ArrayList<>();
+                r1.add(strs[i]);
+                res.add(r1);
+            }
+            else
+            {
+                int c=0;
+                for(int j=0;j<res.size();j++)
+                {
+                    boolean check=anagrams(res.get(j).get(0), strs[i]);
+                    if(check)
+                    {
+                        List<String> innerList = res.get(j);
+                        innerList.add(strs[i]);
+                    }
+                    else
+                        c++;
+                }
+                if(c==res.size())
+                {
+                    List<String> r1=new ArrayList<>();
+                    r1.add(strs[i]);
+                    res.add(r1);
+                }
+            }
+        }
+            
+            return res;
+            
+        }
+    
+    public boolean anagrams(String s, String t)
+    {
+        if(s.length()!=t.length())
+            return false;
+        
+        int c[]=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            int n1=s.charAt(i)-'a';
+            int n2=t.charAt(i)-'a';
+            c[n1]++;
+            c[n2]--;
+        }
+        
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]!=0)
+                return false;
+        }
+        return true;
+    }
+    
+}

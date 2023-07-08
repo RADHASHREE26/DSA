0d86365f03994432562803c1afc369328aef1151
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-07-08 23:51:59 +0530

    Time: 3 ms (92.04%), Space: 56.7 MB (48.91%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,23 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         
-        int slow=nums[0];
-        int fast=nums[0];
-        do{
-            slow=nums[slow];
-            fast=nums[nums[fast]];
-        }while(slow!=fast);
+        int n = nums.length;
         
-        int slow2=nums[0];
-        while(slow!=slow2)
-        {
-            slow=nums[slow];
-            slow2=nums[slow2];
+        for (int i = 0; i <= nums.length - 1; i++) {
+            
+            nums[nums[i] % n] = nums[nums[i] % n]+n;
             
         }
         
-        return slow;
         
+        for (int i = 0; i <nums.length ; i++) {
+            
+            if (nums[i] > 2*n)
+                return i;
+            
+        }
+        
+        return -1;
         
     }
 }
@@ -39,4 +39,25 @@ int n = nums.length;
         }
         
         return -1;
+        */
+
+/*
+Floyd Algo (Tortoise method)
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        
+        int slow2=nums[0];
+        while(slow!=slow2)
+        {
+            slow=nums[slow];
+            slow2=nums[slow2];
+            
+        }
+        
+        return slow;
+        
         */

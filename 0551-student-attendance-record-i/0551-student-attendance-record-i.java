8132bb901b4799948c5b3da12c156cf75426d6c4
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-07-14 23:48:55 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (94.25%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -25,6 +25,34 @@ class Solution {
         }
         
         return true;
-        
     }
 }
+
+/*
+
+int ab=0, l=0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='P')
+            {
+                l=0;
+            }
+            else if(s.charAt(i)=='A')
+            {
+                ab++;
+                l=0;
+                if(ab>=2)
+                    return false;
+            }
+            else if(s.charAt(i)=='L')
+            {
+                l++;
+                if(l>=3)
+                    return false;
+            }
+        }
+        
+        return true;
+        
+        */

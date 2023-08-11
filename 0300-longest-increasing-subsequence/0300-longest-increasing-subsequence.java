105bb3ca83a7b204d004d85a2435c73415a6ae8a
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-08-11 11:43:03 +0530

    Time: 43 ms (69.69%), Space: 42.7 MB (99.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,7 +1,47 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         
-        int n=nums.length;
+        int[] dp = new int[nums.length];
+        
+        dp[0] = 1;
+        int ans = 1;
+        
+        for (int i = 1; i < dp.length; i++) {
+            
+            int max = 0;
+            
+            for (int j = 0; j < i; j++) {
+                
+                if (nums[j] < nums[i]) {
+                    if(dp[j] > max) {
+                        
+                        max = dp[j];
+                        
+                    }
+                }
+                
+            }
+            
+            dp[i] = max + 1;
+            
+            if (dp[i] > ans) ans = dp[i];
+            
+        }
+        
+        return ans;
+
+        
+        
+        
+    }
+    
+}
+
+/*
+
+Bottom up tabulation
+
+int n=nums.length;
         int dp[][] = new int[n+1][n+1];
         for(int i=0;i<=n;i++)
         {
@@ -26,11 +66,7 @@ class Solution {
         
         return dp[0][0];
         
-        
-        
-    }
-    
-}
+        */
 
 /*
 

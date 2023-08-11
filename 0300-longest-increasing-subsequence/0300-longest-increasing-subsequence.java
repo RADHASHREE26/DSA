04f9208c7be0d5c15d7288da09f0fd218c7d5cc6
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-08-11 11:38:47 +0530

    Time: 97 ms (25.35%), Space: 94.2 MB (20.04%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        int n=nums.length;
+        int dp[][] = new int[n+1][n+1];
+        for(int i=0;i<=n;i++)
+        {
+            Arrays.fill(dp[i], 0);
+        }
+        
+        for(int curr=n-1;curr>=0;curr--)
+        {
+            for(int prev=curr-1;prev>=-1;prev--)
+            {
+                //take
+                int take=0;
+                if(prev==-1 || nums[curr]>nums[prev])
+                    take=1+dp[curr+1][curr+1];
+        
+                //not take
+                int notTake=0+dp[curr+1][prev+1];
+                
+                dp[curr][prev+1]=Math.max(take, notTake);
+            }
+        }
+        
+        return dp[0][0];
+        
+        
+        
+    }
+    
+}
+
+/*
+
+TLE (Memoization approach)
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        
+        return helper(nums, nums.length, 0, -1);
+        
+    }
+    
+    public int helper(int[] nums, int n, int curr, int prev)
+    {
+        if(curr==n)
+            return 0;
+        
+        //take
+        int take=0;
+        if(prev==-1 || nums[curr]>nums[prev])
+            take=1+helper(nums, n, curr+1, curr);
+        
+        //not take
+        int notTake=0+helper(nums, n, curr+1, prev);
+        
+        return Math.max(take, notTake);
+        
+        
+    }
+    
+}
+
+*/

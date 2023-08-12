b34744da6ba8323991c5d1c3a9ee545d647f7be5
RADHASHREE BOSE <79526776+RADHASHREE26@users.noreply.github.com>
2023-08-12 20:28:03 +0530

    Time: 15 ms (96.95%), Space: 48.3 MB (59.51%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -11,9 +11,11 @@ class Solution {
         
         for(int i=1;i<n+1;i++)
         {
+            char c1=text1.charAt(i-1);
             for(int j=1;j<m+1;j++)
             {
-                    if(text1.charAt(i-1)==text2.charAt(j-1))
+                char c2=text2.charAt(j-1);
+                    if(c1==c2)
                     dp[i][j]=1+dp[i-1][j-1];
                     else
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
